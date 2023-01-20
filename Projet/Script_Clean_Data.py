ebdfc4e643740c92e02321cfcfbe7ea7aae33308
Lahoucine Hamsek <49314721+RIIDLEY@users.noreply.github.com>
2023-01-20 11:57:12 +0100

    update

--- Projet/Script_Clean_Data.py
+++ Projet/Script_Clean_Data.py
@@ -31,3 +31,46 @@ def first_5000_rows_metropolitan_France(file_name):
 
 
 first_5000_rows_metropolitan_France('DataSetInitCoquelicot.csv')
+
+# import os
+# import csv
+# import random
+# from geopy.geocoders import Nominatim
+# geolocator = Nominatim(user_agent="geoapiExercises")
+
+# # Chemin du dossier d'images
+# folder_path = input("Entrez le chemin du dossier d'images : ")
+
+# # Liste pour stocker les informations des images
+# images_data = []
+
+# # Parcours des images dans le dossier
+# for i, image_name in enumerate(os.listdir(folder_path)):
+#     # Génération aléatoire de nom de plante
+#     plant_name = 'Plante ' + str(random.randint(1, 100))
+#     # Génération aléatoire de date
+#     date = str(random.randint(1, 30)) + '/' + str(random.randint(1, 12)) + '/' + str(random.randint(2000, 2020))
+#     # Génération aléatoire de latitude et longitude
+#     lat = random.uniform(-90, 90)
+#     lon = random.uniform(-180, 180)
+#     # Trouver l'adresse a partir des coordonnées
+#     location = geolocator.reverse((lat,lon), exactly_one=True)
+#     # Si l'adresse est valide
+#     if location is not None:
+#         # extraire les coordonnées
+#         latitude, longitude = location.latitude, location.longitude
+#     else:
+#         latitude, longitude = None, None
+#     # Chemin de l'image
+#     image_path = "https://riidley.github.io/DataVizHAMSEK/Projet/" + os.path.join(folder_path, image_name)
+#     # Ajout des informations de l'image à la liste
+#     images_data.append([i, plant_name, image_path, date, latitude, longitude])
+
+# # Ecriture des données dans un fichier CSV
+# with open('images_data.csv', 'w', newline='') as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerow(['ID', 'nom_plante', 'chemin', 'date','latitude','longitude'])
+#     writer.writerows(images_data)
+
+# print("Fichier CSV créé avec succès!")
+
